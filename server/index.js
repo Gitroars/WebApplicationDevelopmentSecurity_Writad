b21caf5f9e961eaaf839d149b424148f8d29c48c
Arvin <arvin.yuwono@binus.ac.id>
2023-06-01 08:06:15 +0700

    Add Register Routes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import express from "express";
 import cors from "cors";
 //our routes
 import bookRoutes from "./routes/bookRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 connectToDatabase();
@@ -15,7 +16,7 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 app.use("/api/books", bookRoutes);
-
+app.use("/api/users", userRoutes);
 app.listen(port, () => {
   console.log(`server run on port ${port}`);
 });
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,7 +3,7 @@ import User from "../models/User";
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 
-const router = express.router();
+const userRoutes = express.router();
 
 //TODO: redefine expiresIn
 const genToken = (id) => {
@@ -20,3 +20,34 @@ const loginUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid email or password");
   }
 });
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: genToken(user._id),
+    });
+  } else {
+    res.json(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+userRoutes.route("/login").post(loginUser);
+userRoutes.route("/register").post(registerUser);
+
+export default userRoutes;
