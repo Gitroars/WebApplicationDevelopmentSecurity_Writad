c3d7bbc936a144d6e8e20788855658e513fcf8c5
Arvin <arvin.yuwono@binus.ac.id>
2023-06-02 12:27:08 +0700

    Add localhost 5k on api

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { setLoading, setError, userLogin, userLogout ,updateUserProfile,resetUpdate } from "../slices/user";
+import { setLoading, setError, userLogin, userLogout, updateUserProfile, resetUpdate } from "../slices/user";
 
 export const login = (email, password) => async (dispatch) => {
   dispatch(setLoading(true));
@@ -54,32 +54,36 @@ export const register = (name, email, password) => async (dispatch) => {
   }
 };
 
-export const updateProfile=(id,name,email,password)=>async(dispatch, getState)=>{
+export const updateProfile = (id, name, email, password) => async (dispatch, getState) => {
   const {
-    user:{userInfo}
-  }=getState();
-try {
-  const config={
-    headers:{
-      Authroization:`Bearer ${userInfo.token}`,
-      'Content-Type':'application/json',
-    },
-  };
-  const {data}=await axios.get(`/api/users/profile/${id}`,{_id:id,name,email,password},config);
-  dispatch(updateUserProfile(data))
-} catch (error) {
-  dispatch(
-    setError(
-      error.response && error.response.data.message
-        ? error.response.data.message
-        : error.message
-        ? error.message
-        : "An unexpected error has occured. Please try again later"
-    )
-  );
-}
+    user: { userInfo },
+  } = getState();
+  try {
+    const config = {
+      headers: {
+        Authroization: `Bearer ${userInfo.token}`,
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.get(
+      `http://localhost:5000/api/users/profile/${id}`,
+      { _id: id, name, email, password },
+      config
+    );
+    dispatch(updateUserProfile(data));
+  } catch (error) {
+    dispatch(
+      setError(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+          ? error.message
+          : "An unexpected error has occured. Please try again later"
+      )
+    );
+  }
 };
 
-export const resetUpdateSuccess=()=>async(dispatch)=>{
+export const resetUpdateSuccess = () => async (dispatch) => {
   dispatch(resetUpdate());
-}
+};
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,27 +1,27 @@
-import  Jwt  from 'jsonwebtoken';
-import asyncHandler from 'express-async-handler';
-import User from '../models/User.js';
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/User.js";
 
-const protectRoute =asyncHandler(async(req,res,next)=>{
-    let token;
-    if (req.headers.authorization&&req.headers.authorization.startsWith('Bearer')){
-        try{
-            token=req.headers.authorization.split(' ')[1];
-            const decoded=jwt.verify(token,process.env.TOKEN_SECRET);
+const protectRoute = asyncHandler(async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
 
-            req.user=User.findById(decoded.id);
+      req.user = User.findById(decoded.id);
 
-            next();
-        }catch(error){
-            res.status(401)
-            throw new Error('Not Authorized, token failed.');
-        }
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not Authorized, token failed.");
     }
+  }
 
-    if (!token){
-        res.status(401)
-        throw new Error('Not Authorized, no token.');
-    }
-})
+  if (!token) {
+    res.status(401);
+    throw new Error("Not Authorized, no token.");
+  }
+});
 
 export default protectRoute;
