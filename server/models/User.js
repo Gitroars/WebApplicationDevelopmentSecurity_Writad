14d2151689b9aa499520be91c696ef46aa36ac9b
Arvin <arvin.yuwono@binus.ac.id>
2023-06-03 17:20:49 +0700

    Fix isAdmin type

--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false }, // Removed extra 'type' property
+    isAdmin: { type: Boolean, type: String, default: false },
   },
   { timestamps: true }
 );
