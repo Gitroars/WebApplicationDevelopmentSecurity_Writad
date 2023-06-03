d77013ba2f3bd8705ecb76e5f8abf8dfd53a7cec
Arvin <arvin.yuwono@binus.ac.id>
2023-06-03 16:39:03 +0700

    Add localhost for api request

--- client/src/redux/actions/adminActions.js
+++ client/src/redux/actions/adminActions.js
@@ -1,10 +1,5 @@
-import axios from 'axios';
-import {
-  getUsers,
-  userDelete,
-  resetError,
-  setError
-} from '../slices/admin';
+import axios from "axios";
+import { getUsers, userDelete, resetError, setError } from "../slices/admin";
 
 export const getAllUsers = () => async (dispatch, getState) => {
   const {
@@ -15,10 +10,10 @@ export const getAllUsers = () => async (dispatch, getState) => {
     const config = {
       headers: {
         Authorization: `Bearer ${userInfo.token}`,
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     };
-    const { data } = await axios.get('api/users', config);
+    const { data } = await axios.get("http://localhost:5000/api/users", config);
     dispatch(getUsers(data));
   } catch (error) {
     dispatch(
@@ -27,7 +22,7 @@ export const getAllUsers = () => async (dispatch, getState) => {
           ? error.response.data.message
           : error.message
           ? error.message
-          : 'Users could not be fetched.'
+          : "Users could not be fetched."
       )
     );
   }
@@ -42,10 +37,10 @@ export const deleteUser = (id) => async (dispatch, getState) => {
     const config = {
       headers: {
         Authorization: `Bearer ${userInfo.token}`,
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     };
-    const { data } = await axios.delete(`api/users/${id}`, config);
+    const { data } = await axios.delete(`http://localhost:5000/api/users/${id}`, config);
     dispatch(userDelete(data));
   } catch (error) {
     dispatch(
@@ -54,7 +49,7 @@ export const deleteUser = (id) => async (dispatch, getState) => {
           ? error.response.data.message
           : error.message
           ? error.message
-          : 'Users could not be fetched.'
+          : "Users could not be fetched."
       )
     );
   }
