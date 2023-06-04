284b5af3056f05d63bd5aa61fae4ee24a10502ea
kevin matthew T <kmturbobro02@gmail.com>
2023-06-04 18:53:29 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,24 +1,38 @@
 import dotenv from "dotenv";
+dotenv.config();
 
 import connectToDatabase from "./databse.js";
 import express from "express";
 import cors from "cors";
+import path from 'path'
 //our routes
 import bookRoutes from "./routes/bookRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-dotenv.config();
 connectToDatabase();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-const port = process.env.PORT || 5000;
 
 app.use("/api/books", bookRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+const __dirname=path.resolve();
+app.use('uploads',express.static(path.join(__dirname,'/uploads')));
+
+if (process.env.NODE_ENV=='production'){
+  app.use(express.static(path.join(__dirname,'/client/build')));
+
+  app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}
+
+
+
 
 app.listen(port, () => {
   console.log(`server run on port ${port}`);
