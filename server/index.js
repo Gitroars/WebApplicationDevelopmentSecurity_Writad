8a1dfe534f04ed30fe678fe7c9bfd0395e5f7b2c
Arvin <arvin.yuwono@binus.ac.id>
2023-06-05 20:36:34 +0700

    Change API

--- server/index.js
+++ server/index.js
@@ -24,6 +24,15 @@ app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+app.use("uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+}
+
 app.listen(port, () => {
   console.log(`server run on port ${port}`);
 });
